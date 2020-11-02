eb612f34837ce3c707cb3c5b5bd5c53a9d87f75f
Steffen Seckler <steffen.seckler@tum.de>
2020-11-02 11:02:50 +0100

    fixes a couple of errors and warnings

--- doxygen_documentation/onpageload.js
+++ doxygen_documentation/onpageload.js
@@ -1,20 +1,20 @@
-var previous;
+let previous;
 
 function url_content(url) {
     return $.get(url);
 }
 
-function add_link(d, link, text){
-    var link = document.createElement("a");
-    link.href="/doxygen_documentation/git-master/";
-    link.style="margin-right:30px"
-    link.innerText=text;
+function add_link(d, url, text) {
+    let link = document.createElement("a");
+    link.href = url;
+    link.style.marginRight = '30px';
+    link.innerText = text;
     d.append(link);
 }
 
 function create_main_selector() {
 
-    var main_selector = document.getElementById("main_selector");
+    const main_selector = document.getElementById("main_selector");
 
     add_link(main_selector, "/doxygen_documentation/git-master/", "AutoPas Doxygen");
     add_link(main_selector, "/doxygen_documentation_md-flexible/git-master/", "MD-Flexible Doxygen");
@@ -24,26 +24,26 @@ function create_main_selector() {
 
 function create_version_selector() {
 
-    var version_selector = document.getElementById("autopas_version_selector");
+    const version_selector = document.getElementById("autopas_version_selector");
 
-    var url = window.location.href;
+    const url = window.location.href;
 
     // we assume we are on some website from here on out!
-    var indexBase = "../existing_releases.txt";
+    const indexBase = "../existing_releases.txt";
 
     url_content(indexBase).done(function (data) {
-        doc_txt = data.toString();
-        var lines = doc_txt.split('\n');
+        let doc_txt = data.toString();
+        const lines = doc_txt.split('\n');
 
-        for (var i = 0; i < lines.length; i++) {
-            var option = document.createElement("option");
+        for (let i = 0; i < lines.length; i++) {
+            const option = document.createElement("option");
             if (lines[i] === "") {
                 continue;
             }
             option.text = lines[i];
             option.value = lines[i];
             if (url.includes(lines[i])) {
-                option.selected = "selected";
+                option.selected = true;
                 previous = lines[i];
             }
             version_selector.appendChild(option);
@@ -52,16 +52,17 @@ function create_version_selector() {
 }
 
 function autopas_version_change() {
-    var version_selector = document.getElementById("autopas_version_selector");
-    var selected = version_selector.options[version_selector.selectedIndex].value;
-    var oldurl = window.location.href;
-    var newurl = oldurl.replace(previous, selected);
-    window.location.href = newurl;
+    const version_selector = document.getElementById("autopas_version_selector");
+    const selected = version_selector.options[version_selector.selectedIndex].value;
+    const old_url = window.location.href;
+    window.location.href = old_url.replace(previous, selected);
 }
 
 function add_version_change() {
-    var version_selector = document.getElementById("autopas_version_selector");
-    version_selector.addEventListener("change", function(){ autopas_version_change() }, false);
+    const version_selector = document.getElementById("autopas_version_selector");
+    version_selector.addEventListener("change", function () {
+        autopas_version_change()
+    }, false);
 }
 
 function on_page_load() {
